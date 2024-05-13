e61374d16e59381e12657b4200ffe4353fa6d182
Bogusz Kaszowski <bogusz.kaszowski@callstack.com>
2024-05-13 15:25:45 +0200

    Initial Commit

---
+++ BB Quotes/BB_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QuotesApp.swift
+//  BB Quotes
+//
+//  Created by Bogusz Kaszowski on 13/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BB_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
