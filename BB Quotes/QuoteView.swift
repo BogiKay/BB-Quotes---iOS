854320a3bc84720dd395af35a5e6b880f3337841
Bogusz Kaszowski <bogusz.kaszowski@callstack.com>
2024-05-14 22:58:15 +0200

    Make QuoteView to handle BCS show

--- BB Quotes/QuoteView.swift
+++ BB Quotes/QuoteView.swift
@@ -15,55 +15,57 @@ struct QuoteView: View {
     var body: some View {
         GeometryReader { geo in
             ZStack {
-                Image(show.lowercased().replacingOccurrences(of: " ", with: ""))
+                Image(show.lowerNoSpace)
                     .resizable()
                     .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
                     
                 VStack {
-                    Spacer(minLength: 140)
-                    
-                    switch viewModel.status {
-                    case .notStarted:
-                        EmptyView()
-                    case .fetching:
-                        ProgressView()
-                    case .success(let data):
-                        Text("\"\(data.quote.quote)\"")
-                            .minimumScaleFactor(0.5)
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(.black.opacity(0.5))
-                            .clipShape(.rect(cornerRadius: 25))
-                            .padding(.horizontal)
+                    VStack {
+                        Spacer(minLength: 140)
                         
-                        ZStack(alignment: .bottom, content: {
-                            AsyncImage(url: data.character.images[0]) { image in
-                                image.resizable()
-                                    .scaledToFill()
-                            } placeholder: {
-                                ProgressView()
-                            }.frame(width: geo.size.width / 1.1, height: geo.size.height / 1.8)
-                            
-                            
-                            
-                            Text(data.quote.character)
+                        switch viewModel.status {
+                        case .notStarted:
+                            EmptyView()
+                        case .fetching:
+                            ProgressView()
+                        case .success(let data):
+                            Text("\"\(data.quote.quote)\"")
+                                .minimumScaleFactor(0.5)
+                                .multilineTextAlignment(.center)
                                 .foregroundColor(.white)
                                 .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(.ultraThinMaterial)
-                                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
-
-                        }).frame(width: geo.size.width / 1.1, height: geo.size.height / 1.8)
-                            .clipShape(.rect(cornerRadius: 50))
-                    
+                                .background(.black.opacity(0.5))
+                                .clipShape(.rect(cornerRadius: 25))
+                                .padding(.horizontal)
+                            
+                            ZStack(alignment: .bottom, content: {
+                                AsyncImage(url: data.character.images[0]) { image in
+                                    image.resizable()
+                                        .scaledToFill()
+                                } placeholder: {
+                                    ProgressView()
+                                }.frame(width: geo.size.width / 1.1, height: geo.size.height / 1.8)
+                                
+                                
+                                
+                                Text(data.quote.character)
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(.ultraThinMaterial)
+                                    .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                                
+                            }).frame(width: geo.size.width / 1.1, height: geo.size.height / 1.8)
+                                .clipShape(.rect(cornerRadius: 50))
+                            
+                            
+                        case .failed(let error):
+                            EmptyView()
+                        }
                         
-                    case .failed(let error):
-                        EmptyView()
+                        
+                        Spacer()
                     }
-                    
-                    
-                    Spacer()
                     
                     Button {
                         Task {
@@ -74,9 +76,9 @@ struct QuoteView: View {
                         .font(.title)
                         .foregroundColor(.white)
                         .padding()
-                        .background(.breakingBadGreen)
+                        .background(Color("\(show.noSpaces)Button"))
                         .clipShape(.rect(cornerRadius: 7))
-                        .shadow(color: .breakingBadYellow, radius: 2)
+                        .shadow(color: Color("\(show.noSpaces)Shadow"), radius: 2)
                     }
                     
                     
@@ -90,6 +92,6 @@ struct QuoteView: View {
 }
 
 #Preview {
-    QuoteView(show: "Breaking Bad")
+    QuoteView(show: Constants.bbName)
         .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
 }
